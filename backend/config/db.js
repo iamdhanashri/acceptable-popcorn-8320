0a0a01e8f02e1bbfb212b2e01bc99781ab90ed41
zeeshani26 <ilahiamaan606@gmail.com>
2023-03-31 01:19:24 +0530

    added product backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 require("dotenv").config()
 
-const connection=mongoose.connect(process.env.mongourl)
+const connection=mongoose.connect("mongodb+srv://amaan:amaan@cluster0.gwmzyo8.mongodb.net/Project?retryWrites=true&w=majority")
 
 module.exports={
     connection
